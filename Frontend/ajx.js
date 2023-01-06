6847592382ce4d4bf2b0533f5fbff49963c9e188
Saif Ul islam <91761499+syousafzai861@users.noreply.github.com>
2023-01-06 16:25:34 +0500

    full version upto get data

--- Frontend/ajx.js
+++ Frontend/ajx.js
@@ -2,7 +2,7 @@ console.log("Hey Saif ul islam");
 
 console.log("Ajax tutorial in one video");
 
-
+// function of getting data 
 let fetchBtn = document.getElementById('fetch');
 fetchBtn.addEventListener('click', buttonClickHandler)
 
@@ -40,35 +40,10 @@ function buttonClickHandler() {
 
 }
 
-// let popBtn = document.getElementById('popBtn');
-// popBtn.addEventListener('click', popHandler);
+// \
+// 
 
-// function popHandler() {
-//     console.log('You have clicked the pop handler');
-
-//     // Instantiate an xhr object
-//     const xhr = new XMLHttpRequest();
-
-//     // Open the object
-//     xhr.open('GET', 'http://dummy.restapiexample.com/api/v1/employees', true);
-
-
-//     // What to do when response is ready
-//     xhr.onload = function () {
-//         if(this.status === 200){
-//             let obj = JSON.parse(this.responseText);
-//             console.log(obj);
-//             let list = document.getElementById('list');
-//             str = "";
-//             for (key in obj)
-//             {
-//                 str += `<li>${obj[key].employee_name} </li>`;
-//             }
-//             list.innerHTML = str;
-//         }
-//         else{
-//             console.log("Some error occured")
-//         }
+//     /
 //     }
 
 //     // send the request
@@ -78,3 +53,17 @@ function buttonClickHandler() {
 // }
 
 
+// gettting data through ASYNC FUNCTION METHOD 
+// async function getServerSideProps() {
+// // console.log("entered in the function");
+//  let headers = {
+//     Authorization:
+//       "Bearer 108732ab993b1d353a1dd2687b5ecd040d416bcd8061949685410e6288a3ceedb6d7506c562c11b1f4347cfc200dcdeadfae1203f160d71b38a04cbcb0e9dfa23524d0669e417e13b688a579aee43fc7e151a1acbeec3c0e350c52411a2ace11be4907979b27f1631df375f447f09290654d9ad3d768a6309e2c3babcb049cee",
+//   };
+//   let a = await fetch("http://localhost:1337/api/products?populate=*", {headers:headers});
+// //   console.log("api hit kar de ")
+//   let product = await a.json();
+// //   console.log("response mila?")
+//   console.log(product);
+// //   console.log("product show karwa dea done")
+// }
