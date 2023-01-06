69c67bb758ad4ef1907aff4c5d1992b60c2d4f5e
Saif Ul islam <91761499+syousafzai861@users.noreply.github.com>
2023-01-06 19:34:23 +0500

    task completed of mapping the table
    at this stage i have populated the table by getting the api data

--- Frontend/ajx.js
+++ Frontend/ajx.js
@@ -2,47 +2,52 @@ console.log("Hey Saif ul islam");
 
 console.log("Ajax tutorial in one video");
 
-// function of getting data 
-let fetchBtn = document.getElementById('fetch');
-fetchBtn.addEventListener('click', buttonClickHandler)
+// function of getting data
+let fetchBtn = document.getElementById("fetch");
+fetchBtn.addEventListener("click", buttonClickHandler);
 
 function buttonClickHandler() {
-     console.log('You have clicked the fetchBtn');
+  console.log("You have clicked the fetchBtn");
 
-    // Instantiate an xhr object
-    const xhr = new XMLHttpRequest();
-    console.log('On progress');
+  // Instantiate an xhr object
+  const xhr = new XMLHttpRequest();
+  console.log("On progress");
 
-    // Open the object
-    xhr.open('GET', 'http://localhost:1337/api/products?populate=*', true);
-    console.log('On progress');
+  // Open the object
+  xhr.open("GET", "http://localhost:1337/api/products?populate=*", true);
+  console.log("On progress");
 
+  xhr.setRequestHeader(
+    "Authorization",
+    "Bearer 92ca2f13906177b725691b8f457192a173ba5f978a870fd2fbbefe074c7cbce3d4f5c3153f052ac77abbfbfcc8a52495ca68b0816db52e27221aea74d445c5eeaca78f3e8ad9ecc1ec4915db513ab50f3dfadf067316478a73f51acaaa96defd477afe770f2d5f28ce610e5a39e9d442fb8cd05c3bbf09710cd7844e7e254f8c"
+  );
+  console.log("On progress");
 
-    xhr.setRequestHeader('Authorization', 'Bearer 92ca2f13906177b725691b8f457192a173ba5f978a870fd2fbbefe074c7cbce3d4f5c3153f052ac77abbfbfcc8a52495ca68b0816db52e27221aea74d445c5eeaca78f3e8ad9ecc1ec4915db513ab50f3dfadf067316478a73f51acaaa96defd477afe770f2d5f28ce610e5a39e9d442fb8cd05c3bbf09710cd7844e7e254f8c');
-    console.log('On progress');
+  xhr.onreadystatechange = function () {
+    console.log("Some error occured", this);
 
+    if (this.status === 200) {
+      let data = [];
+      let x = JSON.parse(this.responseText);
+      data = x.data;
+      console.log(data);
+      data.map((data, index) => {
+        return (document.getElementById("table_body").innerHTML += `<tr>
+         
+        <td>${data.attributes.name}</td>
+                    <td>${data.attributes.slug}</td>
+                    <td>${data.attributes.description}</td>
+                    <td>${data.attributes.color}</td>
 
-    xhr.onreadystatechange = function () {
-        console.log("Some error occured",this)
-
-        if(this.status === 200){
-
-            // console.log(this.responseText)
-            let x = JSON.parse(this.responseText)
-            console.log(x.data[0].attributes.name)
-            // let file = x.json();
-            // console.log(file)
-            
-        }
-        else{
-            console.log("Some error occured")
-        }
+                    </tr> `);
+      });
+    } else {
+      console.log("Some error occured");
     }
-    xhr.send();
-    
-
+  };
+  xhr.send();
 }
-// gettting data through ASYNC FUNCTION METHOD 
+// gettting data through ASYNC FUNCTION METHOD
 // async function getServerSideProps() {
 // // console.log("entered in the function");
 //  let headers = {
