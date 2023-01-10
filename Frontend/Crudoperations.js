0e35b9245a7068d71648a720cf4b28654e779854
Saif Ul islam <91761499+syousafzai861@users.noreply.github.com>
2023-01-10 18:33:57 +0500

    CRUD APP THROUGH API COMPLETED
    TASK COMPLETED

--- Frontend/Crudoperations.js
+++ Frontend/Crudoperations.js
@@ -4,6 +4,7 @@ const postlist = document.querySelector(".post-list");
 const addpostform = document.querySelector(".add-post-form");
 const titlevalue = document.getElementById("title-value");
 const bodyvalue = document.getElementById("content-value");
+const btnSubmit = document.querySelector(".btn");
 const url = "http://localhost:1337/api/paragraphs";
 
 const renderposts = (posts) => {
@@ -38,6 +39,7 @@ fetch(url, {
     renderposts(data);
   });
 
+//   DELETE/EDIT POST FUNCTION
 postlist.addEventListener("click", (e) => {
   e.preventDefault();
   let deletepost = e.target.id == "deletepost";
@@ -49,12 +51,56 @@ postlist.addEventListener("click", (e) => {
       headers: {
         Authorization:
           "Bearer c3caf881fe46b6b72e7658434c0d60fc84dbbe79ca4d64b8385dbcf57fda026c23fb3d8510aab4aa308e5029628616c157040a1ef6e4af2c347a428dc2dc2df5f6784a2d290f00b469eb7ddc23a4d9eacfa9a221d87c47e4a6a55b2bbff10a09913e2e81647bf5dd36626fd74dc85d4218157d237fcf73a0f8f7f1dbb6d9ac0e",
-          "Content-Type": " application/json",
+        "Content-Type": " application/json",
       },
     })
       .then((res) => res.json())
       .then(() => location.reload());
   }
+
+  if (Editpost) {
+    // console.log("Edit button is pressed")
+    const par = e.target.parentElement;
+    let textcontent = par.querySelector(".card-title").textContent;
+    let bodycontent = par.querySelector(".card-text").textContent;
+    // console.log(textcontent,bodycontent)
+    titlevalue.value = textcontent;
+    bodyvalue.value = bodycontent;
+  }
+
+  // UPDATE FUNC API
+  btnSubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+    let data4 = { data: { Title: titlevalue.value, Content: bodyvalue.value } };
+    // console.log("i am clicked")
+    fetch(`${url}/${id}`, {
+      method: "PUT",
+      headers: {
+        Authorization:
+          "Bearer c3caf881fe46b6b72e7658434c0d60fc84dbbe79ca4d64b8385dbcf57fda026c23fb3d8510aab4aa308e5029628616c157040a1ef6e4af2c347a428dc2dc2df5f6784a2d290f00b469eb7ddc23a4d9eacfa9a221d87c47e4a6a55b2bbff10a09913e2e81647bf5dd36626fd74dc85d4218157d237fcf73a0f8f7f1dbb6d9ac0e",
+        "Content-Type": " application/json",
+      },
+      body: JSON.stringify(data4),
+    }).then((res) => {
+      if (res.status === 200) {
+        fetch(url, {
+          method: "GET",
+          headers: {
+            Authorization:
+              "Bearer c3caf881fe46b6b72e7658434c0d60fc84dbbe79ca4d64b8385dbcf57fda026c23fb3d8510aab4aa308e5029628616c157040a1ef6e4af2c347a428dc2dc2df5f6784a2d290f00b469eb7ddc23a4d9eacfa9a221d87c47e4a6a55b2bbff10a09913e2e81647bf5dd36626fd74dc85d4218157d237fcf73a0f8f7f1dbb6d9ac0e",
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            //   console.log("renderposts",data)
+            renderposts(data);
+          });
+      }
+    })
+    .catch((error) =>{
+        console.log(error)
+    })
+  });
 });
 //   POSTING THE DATA
 addpostform.addEventListener("submit", (e) => {
